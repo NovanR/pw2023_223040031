6840e64ec711ece15f414adc078529b5216e0b4f
NovanR <novanramdan7@gmail.com>
2023-02-18 10:29:04 +0700

    tugas pw 1

---
+++ tugas/tugas1/1a.php
@@ -0,0 +1,31 @@
+<?php
+$topi = '"Topi saya Bundar,';
+$bundar = ' Bundar Topi Saya."';
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 1a</title>
+</head>
+
+<body>
+    <h2 style="font-weight: bold; font-style: italic;">
+        <?php
+
+        echo "$topi" . "$bundar";
+
+        ?>
+    </h2>
+
+
+</body>
+
+</html>
